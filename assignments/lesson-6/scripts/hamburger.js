329a81699a9c9bb3f4babd6be31ba20cff1ba03f
lisaew10 <web18021@byui.edu>
2019-02-16 16:12:27 -0500

    week06

---
+++ assignments/lesson-6/scripts/hamburger.js
@@ -0,0 +1,6 @@
+// to do: expand and collapse menu in small view
+// eslint-disable-next-line require-jsdoc
+function toggleMenu() {
+
+    document.getElementById("menu").classList.toggle("nav");
+}
