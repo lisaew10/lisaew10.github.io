737d045b8cec60584c313b1d4ab42f0db961942e
lisaew10 <web18021@byui.edu>
2019-03-13 21:15:18 -0400

    lesson 10

---
+++ assignments/lesson-10/scripts/weatherapi.js
@@ -0,0 +1,14 @@
+var weatherRequest = new XMLHttpRequest();
+weatherRequest.open('GET', 'http://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=24b87aad01e054f85a32cef48af44129&units=imperial', true);
+
+weatherRequest.send();
+weatherRequest.onload = function () {
+    var weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherRequest);
+
+    document.getElementById('current-temp').innerHTML = weatherData.main.temp;
+
+
+
+}
+
