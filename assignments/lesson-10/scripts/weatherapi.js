4358a2607a23b10d809e5261a83f5058707f217c
lisaew10 <web18021@byui.edu>
2019-03-16 20:02:11 -0400

    update

--- assignments/lesson-10/scripts/weatherapi.js
+++ assignments/lesson-10/scripts/weatherapi.js
@@ -1,3 +1,4 @@
+//current weather
 var weatherRequest = new XMLHttpRequest();
 
 weatherRequest.open('GET', 'https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=24b87aad01e054f85a32cef48af44129&units=imperial', true);
@@ -6,11 +7,36 @@ weatherRequest.send();
 
 weatherRequest.onload = function () {
     var weatherData = JSON.parse(weatherRequest.responseText);
-    console.log(weatherRequest);
+    console.log(weatherData);
+    document.getElementById('mainWeather').innerHTML = weatherData.weather.main;
+    document.getElementById('mainTemp').innerHTML = weatherData.main.temp;
+    document.getElementById('humid').innerHTML = weatherData.main.humidity;
+    // document.getElementById('rain').innerHTML = weatherData.rain.3h;
+    document.getElementById('windSpeed').innerHTML = weatherData.wind.speed;
 
-    document.getElementById('current-temp').innerHTML = weatherData.main.temp;
+    // var iconcode = weatherInfo.weather[0].icon;
+    // var icon_path = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    // document.getElementById('weather_icon').src = icon_path;
+}
+
+
+
+// forecast
+var forecastRequest = new XMLHttpRequest();
+
+forecastRequest.open('GET', 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=24b87aad01e054f85a32cef48af44129&units=imperial', true);
+
+forecastRequest.send();
+
+forecastRequest.onload = function () {
+    var forecastData = JSON.parse(forecastRequest.responseText);
+
+    document.getElementById('temp1').innerHTML = forecastData.list[0].main.temp;
+    document.getElementById('temp2').innerHTML = forecastData.list[1].main.temp;
+    document.getElementById('temp3').innerHTML = forecastData.list[2].main.temp;
+    document.getElementById('temp4').innerHTML = forecastData.list[3].main.temp;
+    document.getElementById('temp5').innerHTML = forecastData.list[4].main.temp;
 
 
 
 }
-
--- assignments/lesson-10/scripts/windchill.js
+++ assignments/lesson-10/scripts/windchill.js
@@ -1,4 +1,4 @@
-var temp = 50;
+var temp = 18;
 var windSpeed = 10;
 var windChill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temp * Math.pow(windSpeed, 0.16);
 document.getElementById("temp").innerHTML = temp + '&deg;';
